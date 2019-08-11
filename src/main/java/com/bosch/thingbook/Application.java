8533d01214c85d7082b3b2684c7b325dba10a75e
Hoang Le Minh (RBVH/ETM2 RBVH/ETM4) <mih1hc@bosch.com>
2019-08-11 10:47:35 +0700

    1st

---
+++ src/main/java/com/bosch/thingbook/Application.java
@@ -0,0 +1,15 @@
+
+package com.bosch.thingbook;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+//481f1d3db6e0cce0b5cdbca39b4bf79071000004  
+	//id:313771  
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
